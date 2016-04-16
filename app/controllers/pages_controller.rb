fbfbccde9eded42421705b0175e6d3953225600d
Paul Bogard <paulbogard@gmail.com>
2016-04-16 18:30:40 -0400

    Add New action to Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,33 @@
 class PagesController < ApplicationController
   def home
+    session[:new_page_number] = nil
   end
   
   def new
+    @page_number = session[:new_page_number]
+    redirect_to root_path if @page_number.nil?
+    if (@page_number.to_i > 1 && Page.where(:page_num => (@page_number.to_i)/2).first.nil?)
+      # Parent page does not exist, so don't create this page
+      redirect_to root_path 
+    end
+    #session[:new_page_number] = nil
+    
+    @page = Page.new
+    
+  end
+  
+  def create
+    
   end
   
   def show
+    @page_number = params[:id]
+    @page = Page.where(:page_num => @page_number).first
+    raise ActiveRecord::RecordNotFound if (@page.nil?) #If this page number doesn't exist, send the user to create it.
+    
+    rescue ActiveRecord::RecordNotFound
+      session[:new_page_number] = @page_number
+      redirect_to new_page_path
   end
   
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ActiveRecord::Base
   validates :page_num, presence: true, uniqueness: true
   validates :story_text, presence: true
-  validates :choice_1, presence: true
-  validates :choice_2, presence: true
+  validates :choice_1, presence: true, length: { maximum: 255 }
+  validates :choice_2, presence: true, length: { maximum: 255 }
 end
